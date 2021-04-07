27d41596be06d8ae982d1eb224b32f90b053bb71
Chhaya Kashyap <chhayak74@gmail.com>
2021-04-08 00:14:42 +0530

    random function optimisation

--- dict.js
+++ dict.js
@@ -1,4 +1,5 @@
-const { search, random, status } = require('./tools.js');
+
+const { search, getRandom, status, printToCli } = require('./tools.js');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,18 +12,18 @@ rl.on('line', (line) => {
   if (line) {
     let dictArgs = line.split(" ");
     if (dictArgs.length == 1) {
-      console.log(random());
+      printToCli(getRandom('prop', getRandom('detail')));
     } else if (dictArgs.length == 2) {
-      console.log(search(dictArgs[1]))
+      printToCli(search(dictArgs[1]));
     } else if (dictArgs.length == 3) {
-      console.log(search(dictArgs[1], dictArgs[2]))
+      printToCli(search(dictArgs[2], dictArgs[1]))
     }
   }
   rl.prompt();
 });
 rl.prompt();
 
-rl.on('exit', () => {
-  console.log("Bbye");
+rl.on('close', () => {
+  console.log("THE ENd!!");
 });
 
--- tools.js
+++ tools.js
@@ -1,12 +1,22 @@
-const words = require('./words.json');
+const wordData = require('./words.json');
+// const words = Object.keys(wordData);
 
 //pick random words
-function random() {
-  // console.log(typeof(words));
-  // console.log(words);
-  let keys = Object.keys(words);
-  // console.log(keys);
-  return words[keys[Math.floor(Math.random()*keys.length)]];
+// function random(type) {
+//   let randWord = wordData[words[Math.floor(Math.random() * words.length)]];
+//   let randProp = randWord[Object.keys(randWord)[Math.floor(Math.random() * Object.keys(randWord).length)]];
+//   if (typeof (randProp) == 'number') {
+//     getRandom(randWord, 'prop');
+//   }
+//   return (type == 'detail' ? randWord : )
+// }
+
+function getRandom(type, obj = wordData) {
+  if(type == 'prop'){
+    delete obj.id;
+  }
+  return obj[Object.keys(obj)[Math.floor(Math.random() * Object.keys(obj).length)]];
+
 }
 
 //maintain current command status
@@ -16,18 +26,21 @@ function status(pid) {
 
 //search for word and return the data
 function search(word, detail) {
+  let searchedWord = wordData[word];
   if (word && detail) {
-    return word + " " + detail;
+    return searchedWord[detail];
   } else if (word) {
-    return word;
+    return searchedWord;
   } else {
     return random();
   }
 }
 
-
-module.exports = {
-  random, status, search
+function printToCli(data) {
+  console.log(data);
 }
 
-//ddd
+
+module.exports = {
+  getRandom, status, search, printToCli
+}
